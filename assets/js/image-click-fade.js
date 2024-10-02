944c4e2675f17f1d2c70f66ee97a77d2848fc6c5
Jiyeon-daisy-kim <Jiyeon-daisy-kim@gmail.com>
2024-10-02 12:10:20 +0900

    first commit for test welcome image

---
+++ assets/js/image-click-fade.js
@@ -0,0 +1,11 @@
+// 이미지 클릭 이벤트 핸들러
+document.querySelector('.welcome-image').addEventListener('click', function() {
+    // 클릭 시 서서히 사라지게 만들기 위해 fade-out 클래스 추가
+    this.classList.add('fade-out');
+    
+     // 1초 후에 새로운 사이트로 이동
+  setTimeout(() => {
+    window.location.href = 'https://daisyeon.com/';  // 원하는 사이트로 이동
+  }, 1000);  // 1초 후 이동
+});
+  
